608a6e24f141c97d85f1fdb6a10994799654ea0d
Dang Lu <38761634+dangnlu18@users.noreply.github.com>
2022-02-24 14:36:32 -0800

    more endpoints added

--- db/vehicles/vehicle-model.js
+++ db/vehicles/vehicle-model.js
@@ -4,6 +4,27 @@ function getVehicle(){
     return db('vehicles')
 }
 
+
+function addVehicle(payload){
+    return db('vehicles').insert(payload)
+}
+
+function getVehicleByModel(year, make, model){
+    return db('vehicles').where({year, make, model})
+}
+
+function findVehicle(payload){
+    let [year, make, model] = payload.split(' ')
+
+    make = make.toLowerCase()
+    model = model.toLowerCase()
+
+    return db('vehicles').where({year, make, model})
+}
+
 module.exports = {
-    getVehicle
+    getVehicle,
+    addVehicle,
+    getVehicleByModel,
+    findVehicle
 }
--- db/vehicles/vehicle-router.js
+++ db/vehicles/vehicle-router.js
@@ -1,13 +1,52 @@
 const express = require('express')
 
-const cars = require('./vehicle-model')
+const vehicles = require('./vehicle-model')
 
 const router = express.Router()
 
 router.get('/', async(req,res,next) =>{
     try{
-        const car = await cars.getVehicle()
+        const car = await vehicles.getVehicle()
         res.json(car)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+
+router.post('/', async(req,res,next)=>{
+    try{
+        const payload = req.body
+        const newCar = await vehicles.addVehicle(payload)
+        res.status(201).json({message: "successfully added"})
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.get('/:year/:make/:model', async(req,res,next) =>{
+    try{
+        const {year, make, model} = req.params
+        const [car] = await vehicles.getVehicleByModel(year, make, model)
+        res.json(car)
+    }
+    catch(err){
+        next(err)
+    }
+})
+
+router.get('/:findVehicle', async(req,res,next)=>{
+    try{
+        const payload = req.query.body
+        console.log(payload)
+        const [findVehicle] = await vehicles.findVehicle(payload)
+        console.log(findVehicle)
+        if (findVehicle === 'undefined'){
+            res.status(404)
+        }
+        res.status(200).json({car: findVehicle})
     }
     catch(err){
         next(err)
